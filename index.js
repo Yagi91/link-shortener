d9d29cd62a5548560a859dd7bdc906bf7886b4fa
Yagi91 <bryantimah@gmail.com>
2023-03-03 01:41:10 +0100

    check if the url is valid

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const dns = require("node:dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,9 +19,27 @@ app.get("/", function (req, res) {
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(bodyParser.json());
+
 // Your first API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
+});
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+
+  if (
+    dns.lookup(url, (err, address, family) => {
+      console.log("address: %j family: IPv%s", address, family);
+      if (err) return err;
+      return address;
+    })
+  ) {
+    return res.json({ error: "invalid URL" });
+  }
+
+  res.json({ original_url: url, short_url: 1 });
 });
 
 app.listen(port, function () {
